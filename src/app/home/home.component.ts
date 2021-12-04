31392b6c79d4fb135c398914f36817fe57209adf
Rohit Patle <rohit.patle@atos.net>
2021-12-04 23:52:04 +0530

    Final Push with Changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Patients } from '../patients';
 import { PatientDataService } from '../patient-data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,7 @@ export class HomeComponent implements OnInit {
 
   patients: any = [];
 
-  constructor(private patientData: PatientDataService) {
+  constructor(private patientData: PatientDataService, private router: Router) {
 
   }
 
